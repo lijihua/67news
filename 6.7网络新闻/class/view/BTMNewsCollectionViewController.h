e9b8006fbf93173a32818ee827b7a735bc51a347
Macx <Macx@macxdemacbook-pro-2.local>
2016-05-07 20:04:39 +0800

    第二次提交项目

---
+++ 6.7网络新闻/class/view/BTMNewsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTMNewsCollectionViewController.h
+//  6.7网络新闻
+//
+//  Created by Macx on 16/5/7.
+//  Copyright © 2016年 1020520. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTMNewsCollectionViewController : UICollectionViewController
+
+@end
